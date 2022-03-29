277819371da8aeaf96d0a08a53650b50bb04fdc6
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2022-03-29 14:15:08 +0200

    fix: clean up

--- source/php/App.php
+++ source/php/App.php
@@ -6,35 +6,7 @@ class App
 {
     public function __construct()
     {
-        add_action('admin_enqueue_scripts', array($this, 'enqueueStyles'));
-        add_action('admin_enqueue_scripts', array($this, 'enqueueScripts'));
         add_action('init', array($this, 'registerPostType'), 9);
-    }
-
-    /**
-     * Enqueue required style
-     * @return void
-     */
-    public function enqueueStyles()
-    {
-        wp_register_style(
-            'api-kiosk-model-viewer-css',
-            API_KIOSK_MODEL_VIEWER_URL . '/dist/' .
-            \ApiKioskModelViewer\Helper\CacheBust::name('css/api-kiosk-model-viewer.css')
-        );
-    }
-
-    /**
-     * Enqueue required scripts
-     * @return void
-     */
-    public function enqueueScripts()
-    {
-        wp_register_script(
-            'api-kiosk-model-viewer-js',
-            API_KIOSK_MODEL_VIEWER_URL . '/dist/' .
-            \ApiKioskModelViewer\Helper\CacheBust::name('js/api-kiosk-model-viewer.js')
-        );
     }
 
     public function registerPostType()
